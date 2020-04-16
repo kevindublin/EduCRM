25367a1b4f27ef3e37e18e8951ab6dbe17a51b7e
Kevin Dublin <kevin@kevindublin.com>
2020-04-15 21:28:38 -0700

    add API endpoints in view, update and migrate models, initial backend setup

--- EduCRM/urls.py
+++ EduCRM/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from students import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/students/$', views.students_list),
+    url(r'^api/students/(?P<pk>[0-9]+)$', views.students_detail),
 ]
--- students/models.py
+++ students/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField("First Name", max_length=50)
+    last_name = models.CharField("Last Name", max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+    address_two = models.TextField(blank=True, null=True)
+    city = models.CharField("City", max_length=50, null=True)
+    state = models.CharField("State", max_length=30, null=True)
+    zip_code = models.CharField(max_length=7, null=True)
+    bio = models.TextField(blank=True, null=True)
+    created = models.DateTimeField("Created at", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        db_table = 'contact_info'
+        managed = True
+        verbose_name = 'student'
+        verbose_name_plural = 'students'
